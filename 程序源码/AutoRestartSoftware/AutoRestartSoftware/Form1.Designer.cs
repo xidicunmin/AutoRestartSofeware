6b030ed70e859920581f37e907accf50c1547351
xidicunmin <redwangshifu@sina.com>
2020-11-07 14:38:30 +0800

    完成配置文件部分

---
+++ 程序源码/AutoRestartSoftware/AutoRestartSoftware/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace AutoRestartSofeware
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSoftwarePath = new System.Windows.Forms.TextBox();
+            this.lblSofewarePath = new System.Windows.Forms.Label();
+            this.btnSelectSoftwarePath = new System.Windows.Forms.Button();
+            this.chkStartWithWindows = new System.Windows.Forms.CheckBox();
+            this.btnSaveConfig = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSoftwarePath
+            // 
+            this.txtSoftwarePath.Enabled = false;
+            this.txtSoftwarePath.Location = new System.Drawing.Point(84, 20);
+            this.txtSoftwarePath.Name = "txtSoftwarePath";
+            this.txtSoftwarePath.Size = new System.Drawing.Size(216, 21);
+            this.txtSoftwarePath.TabIndex = 0;
+            // 
+            // lblSofewarePath
+            // 
+            this.lblSofewarePath.AutoSize = true;
+            this.lblSofewarePath.Location = new System.Drawing.Point(13, 23);
+            this.lblSofewarePath.Name = "lblSofewarePath";
+            this.lblSofewarePath.Size = new System.Drawing.Size(65, 12);
+            this.lblSofewarePath.TabIndex = 1;
+            this.lblSofewarePath.Text = "程序路径：";
+            // 
+            // btnSelectSoftwarePath
+            // 
+            this.btnSelectSoftwarePath.Location = new System.Drawing.Point(306, 18);
+            this.btnSelectSoftwarePath.Name = "btnSelectSoftwarePath";
+            this.btnSelectSoftwarePath.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectSoftwarePath.TabIndex = 2;
+            this.btnSelectSoftwarePath.Text = "选择程序";
+            this.btnSelectSoftwarePath.UseVisualStyleBackColor = true;
+            this.btnSelectSoftwarePath.Click += new System.EventHandler(this.btnSelectSoftwarePath_Click);
+            // 
+            // chkStartWithWindows
+            // 
+            this.chkStartWithWindows.AutoSize = true;
+            this.chkStartWithWindows.Location = new System.Drawing.Point(15, 51);
+            this.chkStartWithWindows.Name = "chkStartWithWindows";
+            this.chkStartWithWindows.Size = new System.Drawing.Size(72, 16);
+            this.chkStartWithWindows.TabIndex = 3;
+            this.chkStartWithWindows.Text = "开机启动";
+            this.chkStartWithWindows.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveConfig
+            // 
+            this.btnSaveConfig.Location = new System.Drawing.Point(306, 47);
+            this.btnSaveConfig.Name = "btnSaveConfig";
+            this.btnSaveConfig.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveConfig.TabIndex = 4;
+            this.btnSaveConfig.Text = "保存";
+            this.btnSaveConfig.UseVisualStyleBackColor = true;
+            this.btnSaveConfig.Click += new System.EventHandler(this.btnSaveConfig_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 81);
+            this.Controls.Add(this.btnSaveConfig);
+            this.Controls.Add(this.chkStartWithWindows);
+            this.Controls.Add(this.btnSelectSoftwarePath);
+            this.Controls.Add(this.lblSofewarePath);
+            this.Controls.Add(this.txtSoftwarePath);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(410, 120);
+            this.MinimumSize = new System.Drawing.Size(410, 120);
+            this.Name = "Form1";
+            this.Text = "退出自动重启";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSoftwarePath;
+        private System.Windows.Forms.Label lblSofewarePath;
+        private System.Windows.Forms.Button btnSelectSoftwarePath;
+        private System.Windows.Forms.CheckBox chkStartWithWindows;
+        private System.Windows.Forms.Button btnSaveConfig;
+    }
+}
+
---
+++ 程序源码/AutoRestartSoftware/AutoRestartSoftware/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace AutoRestartSofeware
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                txtSoftwarePath.Text = GetValue("softwarePath");
+                bool startWithWindows;
+                chkStartWithWindows.Checked = bool.TryParse(GetValue("startWithWindows"), out startWithWindows) ? startWithWindows : false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("初始化异常："+ex);
+            }
+        }
+
+        private void btnSelectSoftwarePath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofg = new OpenFileDialog();
+            ofg.Filter = "可执行文件|*.exe";
+            if (ofg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtSoftwarePath.Text = ofg.FileName;
+            }
+        }
+
+        private void btnSaveConfig_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SetValue("softwarePath", txtSoftwarePath.Text);
+                SetValue("startWithWindows", chkStartWithWindows.Checked.ToString());
+                MessageBox.Show("保存成功");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("保存异常："+ex);
+            }
+        }
+
+        /// <summary>  
+        /// 写入值  
+        /// </summary>  
+        /// <param name="key"></param>  
+        /// <param name="value"></param>  
+        public static void SetValue(string key, string value)
+        {
+            //增加的内容写在appSettings段下 <add key="RegCode" value="0"/>  
+            System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (config.AppSettings.Settings[key] == null)
+            {
+                config.AppSettings.Settings.Add(key, value);
+            }
+            else
+            {
+                config.AppSettings.Settings[key].Value = value;
+            }
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");//重新加载新的配置文件   
+        }
+
+        /// <summary>  
+        /// 读取指定key的值  
+        /// </summary>  
+        /// <param name="key"></param>  
+        /// <returns></returns>  
+        public static string GetValue(string key)
+        {
+            System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (config.AppSettings.Settings[key] == null)
+                return "";
+            else
+                return config.AppSettings.Settings[key].Value;
+        }  
+
+
+    }
+}
